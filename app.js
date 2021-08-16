52061d1297d4a3abbfd7ed5c57878bf63c9a1ffa
Fahad <fahadmohd386@gmail.com>
2021-08-16 23:02:22 +0530

    final app

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://serene-refuge-94010.herokuapp.com/auth/google/secrets",
+    callbackURL: "http://localhost:3000/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
